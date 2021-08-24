0d55364aa8d011be13e7f91466ed7f356f127ed8
Messquerade <annac.klingberg@gmail.com>
2021-08-24 11:54:42 -0700

    add rightclick event to delete
    Co-authored-by: Nathan Fletcher <nathan.fletcher@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -31,10 +31,11 @@ AddressBook.prototype.deleteContact = function(id) {
 
 
 // Business Logic for Contacts -------
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, workEmailAddress, personalEmailAddress) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
+  this.emailAddresses = {"work": workEmailAddress, "home": personalEmailAddress};
 }
 
 Contact.prototype.fullName = function() {
@@ -51,14 +52,64 @@ Contact.prototype.update = function(newName, newLastName, newPhoneNumber) {
 // User Interface Logic ---------
 let addressBook = new AddressBook();
 
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+}
+
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function() {
+    showContact(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function() {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
+}
+
+function showContact(contactId) {
+  const contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  Object.keys(contact.emailAddresses).forEach(key => {
+    $("." + key + "-email-address").html(contact.emailAddresses[key]);
+
+  })
+  // $(".work-email-address").html(contact.emailAddresses["work"]);
+  // $(".home-email-address").html(contact.emailAddresses["personal"]);
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
+}
+
 $(document).ready(function() {
+  attachContactListeners();
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
     const inputtedFirstName = $("input#new-first-name").val();
     const inputtedLastName = $("input#new-last-name").val();
     const inputtedPhoneNumber = $("input#new-phone-number").val();
-    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    const inputtedWorkEmailAddress = $("input#new-work-email").val();
+    const inputtedHomeEmailAddress = $("input#new-home-email").val();
+    
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-phone-number").val("");
+    $("input#new-work-email").val("");
+    $("input#new-work-email").val("");
+
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedWorkEmailAddress, inputtedHomeEmailAddress);
     addressBook.addContact(newContact);
-    console.log(addressBook.contacts);
+    displayContactDetails(addressBook);
+    console.log(newContact);
   });
 });
